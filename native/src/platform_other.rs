5745ee47b89bf9b817e109fc64cf95fe275ff5fb
Neeme Praks <neeme@praks.net>
2026-01-02 10:47:34 +0200

    Update license and license headers

--- native/src/platform_other.rs
+++ native/src/platform_other.rs
@@ -1,4 +1,4 @@
-// Copyright 2025 Neeme Praks
+// Copyright (C) 2026 Neeme Praks
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
